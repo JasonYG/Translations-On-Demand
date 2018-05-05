a348b995dcd8e2d8ae44c6296709bb6cd1bb2ef9
Raymond Qu <raymond101.qu@gmail.com>
2018-05-05 19:36:27 -0400

    cleaner

--- translate-bot/translate.py
+++ translate-bot/translate.py
@@ -1,22 +1,6 @@
 import os
 from google.cloud import translate
 
-##def translate_text(text, target = 'en'):
-##
-##    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gc-key.json"
-##    
-##    translate.client = translate.Client()
-##    result = translate_client.translate(text, target_language = target)
-##
-##    print('Text: ', result['input'])
-##    print('Translation', result['translatedText'])
-##    print('Detected source language: ', result['detectedSourceLanguage'])
-##
-##
-##ex = "j'aime le chien"
-##
-##translate_text(ex)
-
 # Imports the Google Cloud client library
 from google.cloud import translate
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gc-key.json"
