486c8d503c0041d30a948d7ded369a04c77af092
Jason Guo <jasonyzguo@gmail.com>
2018-05-06 11:13:43 -0400

    Add in multiple language support

--- php webserver/translate.php
+++ php webserver/translate.php
@@ -1,17 +1,17 @@
 <?php
-
-
 function translate() {
 $apiKey = 'AIzaSyCABvyqIG-VDjACqOu50_yIBciDHSflNqQ';
 
 # The target language
-$target = 'fr';
+$language_file = fopen("language.txt", "r");
+$target = fgets($language_file);
 
+//$target = 'fr';
 $to_translate_file = fopen('to_translate.txt', 'r') or die("unable to open file");
 $translated_text = fgets($to_translate_file);
 fclose($to_translate_file);
 
-$url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($translated_text) . '&source=en&target=fr';
+$url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($translated_text) . '&target='.$target;
 
 $handle = curl_init($url);
 curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
