1e91d5f984cc5f9357db8303e487c157953f4c6c
Jason Guo <jasonyzguo@gmail.com>
2018-05-05 23:34:44 -0400

    Implement translate function

--- php webserver/translate.php
+++ php webserver/translate.php
@@ -1,28 +1,37 @@
 <?php
-# Includes the autoloader for libraries installed with composer
-require __DIR__ . '/vendor/autoload.php';
+function translate() {
+	# Includes the autoloader for libraries installed with composer
+	require __DIR__ . '/vendor/autoload.php';
 
-# Imports the Google Cloud client library
-use Google\Cloud\Translate\TranslateClient;
+	# Imports the Google Cloud client library
+	use Google\Cloud\Translate\TranslateClient;
 
-# Your Google Cloud Platform project ID
-$projectId = 'translate-chatbo-1525555964305';
+	# Your Google Cloud Platform project ID
+	$projectId = 'translate-chatbo-1525555964305';
 
-# Instantiates a client
-$translate = new TranslateClient([
-    'projectId' => $projectId
-]);
+	# Instantiates a client
+	$translate = new TranslateClient([
+	    'projectId' => $projectId
+	]);
 
-# The text to translate
-$text = 'Hello, world!';
-# The target language
-$target = 'fr';
+	# Read from file, text to be translated
+	$to_translate = fopen("chat.txt", "r");
+	$text = fgets($to_translate);
+	fclose($to_translate);
 
-# Translates some text into Russian
-$translation = $translate->translate($text, [
-    'target' => $target
-]);
+	# The target language
+	$target = 'fr';
 
-echo 'Text: ' . $text . '
-Translation: ' . $translation['text'];
+	# Translates some text into Russian
+	$translation = $translate->translate($text, [
+	    'target' => $target
+	]);
+
+	# Write the translated text to the chat
+	$to_translate = fopen("chat.txt", "w");
+	fwrite($to_translate, $translation['text']);
+
+	echo 'Text: ' . $text . '
+	Translation: ' . $translation['text'];
+}
 ?>
